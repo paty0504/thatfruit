3c31579857b2612f23d50afb7fc6d1bb3e334399
Nguyễn Tiến Thành <37099218+paty0504@users.noreply.github.com>
2019-10-19 12:02:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-@app.route('/home')
-def index():
-    return render_template('index.html')
+
 @app.route('/news')
 def news():
     return render_template('news.html')    
